2c481cdc02b00b7e015657a83b676f2fcf7ba546
shmailez <60861135+shmailez@users.noreply.github.com>
2023-05-12 17:22:59 +0600

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -8,7 +8,7 @@ import {
     TOGGLE_TAB
  } from './type'
 
-const API_URL = `http://localhost:8000`
+const API_URL = `https://just-todo-mern.onrender.com`
 
 export const addNewTodo = (data) => async (dispatch) => {
     try{
